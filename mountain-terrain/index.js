650fb30fefe008978587fb48ef2355aa37529f48
古月月月 <75478082+Lanmai934@users.noreply.github.com>
2025-09-26 13:37:11 +0800

    起伏的山脉地形

---
+++ mountain-terrain/index.js
@@ -0,0 +1,40 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import {mesh, updatePosition } from './mesh.js';
+
+
+
+const scene = new THREE.Scene();
+// const axesHelper=new THREE.AxesHelper(200);
+// scene.add(axesHelper);
+
+scene.add(mesh);
+
+const pointLight = new THREE.PointLight(0xffffff, 10000);
+pointLight.position.set(80, 80, 80);
+scene.add(pointLight);
+
+const w = window.innerWidth;
+const h = window.innerHeight;
+
+
+const camera = new THREE.PerspectiveCamera(60, w / h, 1, 1000);
+camera.position.set(450, 250, 100);
+camera.lookAt(0, 0, 0);
+
+
+
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(w, h);
+
+function render() {
+    updatePosition();
+    mesh.rotateZ(0.003);
+    renderer.render(scene, camera);
+    requestAnimationFrame(render);
+}
+render();
+
+document.body.append(renderer.domElement);
+const controls = new OrbitControls(camera, renderer.domElement);
---
+++ mountain-terrain/mesh.js
@@ -0,0 +1,37 @@
+import * as THREE from 'three';
+import { createNoise2D } from "simplex-noise";
+const noise2D = createNoise2D();
+
+const geometry=new THREE.PlaneGeometry(3000,3000,100,100);
+// const position=geometry.attributes.position;
+// for(let i=0;i<position.count;i++){
+//     const x=position.getX(i);
+//     const y=position.getY(i);
+
+
+//     //缩放控制粗糙度noise2D(x / 100, y / 100)
+//     //noise2D(x/scale, y/scale) 控制“纹理大小”，scale 越大 → 越平滑
+//     position.setXYZ(i,x,y,(noise2D(x / 100, y / 300) * 50));
+
+// }
+
+const material=new THREE.MeshBasicMaterial({color: new THREE.Color('orange'),wireframe:true});
+const mesh=new THREE.Mesh(geometry,material);
+// mesh.rotateX(Math.PI/2)
+console.log(mesh);
+
+
+
+function updatePosition(){
+    const position=geometry.attributes.position;
+    for(let i=0;i<position.count;i++){
+        const x=position.getX(i);
+        const y=position.getY(i);
+        const z=noise2D(x/300, y/300)*50;
+        // 动态变化
+        const sinNum = Math.sin(Date.now() * 0.002 + x * 0.05) * 10;
+        position.setXYZ(i,x,y,z+sinNum);
+    }
+   position.needsUpdate=true;
+}
+export {updatePosition ,mesh};
